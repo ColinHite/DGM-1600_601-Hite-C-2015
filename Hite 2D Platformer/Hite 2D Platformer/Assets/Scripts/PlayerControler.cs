e79526266abea03c625d7ecd2d20870cccd76229
unknown <10712935@CS514-15.ad.uvu.edu>
2015-09-21 19:53:40 -0600

    Added Debug.log"s" to the script to see what's going on.

--- Hite 2D Platformer/Hite 2D Platformer/Assets/Scripts/PlayerControler.cs
+++ Hite 2D Platformer/Hite 2D Platformer/Assets/Scripts/PlayerControler.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerControler : MonoBehaviour {
+public class PlayerControler : MonoBehaviour 
+{
 
 	public float moveSpeed;
 	public float jumpHeight;
@@ -9,18 +10,22 @@ public class PlayerControler : MonoBehaviour {
 	void Update () 
 	{
 		//Move Right
-		if(Input.GetKey (KeyCode.D)){		
+		if(Input.GetKey (KeyCode.D))
+		{		
 			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
-
-	}
+			Debug.Log("I moved right");
+		}
 		//Move Left
-		if(Input.GetKey (KeyCode.A)){		
+		if(Input.GetKey (KeyCode.A))
+		{		
 			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
-	
-	}
+			Debug.Log("I moved left");
+		}
 		// Jump
-		if(Input.GetKeyDown (KeyCode.Space)){		
+		if(Input.GetKeyDown (KeyCode.Space))
+		{		
 			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+			Debug.Log("I jumped");
+		}
 	}
-}
 }
