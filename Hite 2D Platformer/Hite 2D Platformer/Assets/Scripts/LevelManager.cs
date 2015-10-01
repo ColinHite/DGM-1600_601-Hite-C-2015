fce5cefe0c51e1086c7f9ed22769c31f219c2bf1
unknown <10712935@CS514-15.ad.uvu.edu>
2015-09-30 20:15:50 -0600

    Started CheckPoint/Respawn.

---
+++ Hite 2D Platformer/Hite 2D Platformer/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public GameObject currentCheckPoint;
+	private PlayerControler player;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<PlayerControler> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void RespawnPlayer()
+	{
+		Debug.Log ("Player Respawned");
+		player.transform.position = currentCheckPoint.transform.position;
+	}
+
+}
