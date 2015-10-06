625eaa4caeff40c9f46cf92144d203078d0af8a6
unknown <10712935@CS514-09.ad.uvu.edu>
2015-10-05 19:59:52 -0600

    This Commit is getting me up to speed with the death trigger. I also added a useless script folder for script examples as long as this 2Dplatformer overlaps script pass off.

---
+++ Hite 2D Platformer/Hite 2D Platformer/Assets/Scripts/NonUsedScripts/WhileLoop.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhileLoop : MonoBehaviour {
+
+
+	int BurgersEatin = 3;
+
+
+	void Start () {
+		while (BurgersEatin > 0) {
+			Debug.Log ("I've have eatin a burger.");
+			BurgersEatin--;
+		}
+	}
+}
