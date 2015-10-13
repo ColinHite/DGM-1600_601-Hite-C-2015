9a217c0633d6a79f804fc145b7283ae0e654006e
unknown <10712935@CS514-10.ad.uvu.edu>
2015-10-12 20:11:02 -0600

    Got the check point system to work.

--- Hite 2D Platformer/Hite 2D Platformer/Assets/Scripts/CheckPoint.cs
+++ Hite 2D Platformer/Hite 2D Platformer/Assets/Scripts/CheckPoint.cs
@@ -1,15 +1,27 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class CheckPoint : MonoBehaviour {
 
+    public LevelManager levelManager;
+    
 	// Use this for initialization
 	void Start () {
-	
+	levelManager = FindObjectOfType<LevelManager> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 	
 	}
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+     if(other.name == "Hero")
+     {
+      levelManager.currentCheckPoint = gameObject;
+         Debug.Log("Activated Checkpoint" + transform.position);
+     }
+    }
 }
