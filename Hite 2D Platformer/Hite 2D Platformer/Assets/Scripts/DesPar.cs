feb7581e7554c165e46628d95a2c00b56158dde0
unknown <10712935@CS514-11.ad.uvu.edu>
2015-11-02 20:13:39 -0700

    Got some particles into the game and made a death delay.

---
+++ Hite 2D Platformer/Hite 2D Platformer/Assets/Scripts/DesPar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DesPar : MonoBehaviour {
+
+
+	//Destroys Non-Used Particles
+	//
+	//
+
+	private ParticleSystem myParticleSystem;
+
+
+
+	// Use this for initialization
+	void Start () {
+		myParticleSystem = GetComponent<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	if (myParticleSystem.isPlaying) 
+			return;
+
+			Destroy (gameObject);
+	}
+}
