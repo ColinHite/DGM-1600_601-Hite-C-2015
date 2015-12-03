c51269ef4a089749bd009e51b9ac8181b45783e9
unknown <10712935@CS514-11.ad.uvu.edu>
2015-12-02 20:15:41 -0700

    Got a shooting fireball.

---
+++ Hite 2D Platformer/Hite 2D Platformer/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+
+	public Transform firePoint;
+
+	public GameObject projectile;
+
+
+	void Start () 
+	{
+	
+	}
+	
+
+	void Update () 
+	{
+	if (Input.GetKeyDown (KeyCode.Mouse0)) 
+		{
+			Instantiate(projectile, firePoint.position, firePoint.rotation);
+		}
+
+	}
+}
---
+++ Hite 2D Platformer/Hite 2D Platformer/Assets/Scripts/ProMoveDes.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProMoveDes : MonoBehaviour {
+
+	public int speed;
+	public PlayerControler player;
+
+	public GameObject enemyDeath;
+
+	public GameObject projectileParticle;
+
+	public int pointsForKill;
+
+	void Start () 
+	{
+		player = FindObjectOfType<PlayerControler> ();
+
+		if (player.transform.localScale.x < 0) 
+		{
+			speed = -speed;
+		}
+	}
+	
+
+	void Update () 
+	{
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (speed, GetComponent<Rigidbody2D>().velocity.y);
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "enemy") 
+		{
+			Instantiate(enemyDeath, other.transform.position, other.transform.rotation);
+			Destroy (other.gameObject);
+			ScoreManager.AddPoints (pointsForKill);
+		}
+		Instantiate(projectileParticle, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+}
