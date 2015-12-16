2127a6b8156410c4ca4f4a04e15bc9f170fc77ad
ColinHite <chit9353@gmail.com>
2015-12-16 15:11:23 -0700

    Did a little tweeking and fixed a few things. Still a couple things arent working like a title. For what ever reason it doesnt like to work for me. Maybe its my version IDk. In any case there should now be a completed build and everything should be working. YAY!!!

---
+++ Hite 2D Platformer/Hite 2D Platformer/Assets/Scripts/EndLevel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndLevel : MonoBehaviour {
+
+	//Screen Overlay Object
+	public GameObject win;
+	
+
+	//Finds the player to complete the level.
+	void OnTriggerEnter2D(Collider2D col)
+	{
+
+		if (col.gameObject.tag == "Player") 
+		{
+
+			win.SetActive(true);
+
+		}
+
+	}
+}
